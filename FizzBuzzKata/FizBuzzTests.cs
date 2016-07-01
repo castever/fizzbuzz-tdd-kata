c896326f334ab49fb1c5b200cceeb2282082119e
Chad Stever <castever@gmail.com>
2016-07-01 15:32:52 -0500

    first commit; first test; should return number that is not divisible by 5 or 3 as string representation of that number

---
+++ FizzBuzzKata/FizBuzzTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzKata
+{
+    [TestClass]
+    public class FizBuzzTests
+    {
+
+        [TestMethod]
+        public void TestReturnsNumbers()
+        {
+            string expected = "1";
+            int number = 1;
+            FizzBuzz fizzBuzz = new FizzBuzz(number);
+            Assert.AreEqual(expected, fizzBuzz.Value());
+        }
+    }
+}
---
+++ FizzBuzzKata/FizzBuzz.cs
@@ -0,0 +1,17 @@
+﻿namespace FizzBuzzKata
+{
+    internal class FizzBuzz
+    {
+        private int _number;
+
+        public FizzBuzz(int number)
+        {
+            this._number = number;
+        }
+
+        public string Value()
+        {
+            return _number.ToString();
+        }
+    }
+}
